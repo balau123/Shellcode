c1665c37615787d9478f2ab5b2207a7ca0b300ac
balau123 <thai.itplus@gmail.com>
2017-10-08 11:21:33 -0700

    Fix bugs

--- shellcodeAsArray/sa.py
+++ shellcodeAsArray/sa.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-from isis import chunk
-
+#from isis import chunk
 from sys import argv
 if len(argv)<2:
     print 'Usage: {} <shellcode> [<bytes/line>]'.format(argv[0])
@@ -11,11 +10,10 @@ if len(argv)>2:
     LINE_LEN=int(argv[2])
 
 shellcode = file(argv[1]).read()
-hex_bytes=[i.encode('hex').zfill(2) for i in shellcode]
+hex_bytes="\\x"
+hex_bytes+="\\x".join([i.encode('hex').zfill(2) for i in shellcode])
+print "Shellcode=",hex_bytes
+print "Byte:\n",len(shellcode)
 
-print 'shellcode = (',
-for i in chunk(hex_bytes, LINE_LEN):
-    print '"{}"'.format( r'\x'+r'\x'.join(i))    
-print ')'
 
 
